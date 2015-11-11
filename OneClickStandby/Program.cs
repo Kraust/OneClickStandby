49c3e7c050558e6663f62cb7856015f3bc6dbe39
Robert Dupuis <secretdragoon@gmail.com>
2015-11-10 19:37:21 -0500

    Initial Commit

---
+++ OneClickStandby/Program.cs
@@ -0,0 +1,20 @@
+﻿// One Click Standby Mode
+// Requested by PCH on 2015-11-10
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace OneClickStandby
+{
+  class Program
+  {
+    [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+    public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Entering Standby Mode...");
+      SetSuspendState(false, true, true);
+    }
+  }
+}
